149f2037a66d905a20fc8c1b45c0b2dc6c74eda0
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-06-28 16:25:16 -0300

    Dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
 export function App() {
   const counter = useSelector((state) => state.counter);
-
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+  const addBy = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <div>
       <h1>teste</h1>
       <h1>{counter}</h1>
+      <button onClick={() => increment()}>increment</button>
+      <button onClick={() => decrement()}>decrement</button>
+      <button onClick={() => addBy()}>Add by 10</button>
     </div>
   );
 }
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,6 +5,15 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+  if (action.type === "ADD") {
+    return { counter: state.counter + action.payload };
+  }
   return state;
 };
 
